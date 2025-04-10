5ba4e12ddc38d4eaa7f5489be8a395bc95aa2d37
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-04-10 23:23:26 +0200

    completed ex2

--- conditional-statements-and-loops.js
+++ conditional-statements-and-loops.js
@@ -15,9 +15,7 @@ if(temperature < 0){
     console.log("It's warm.")
 }
 
-let temperature2 = 14
-
-switch (temperature) {
+switch (true) {
     case (temperature < 0):
         console.log("It's freezing!")
         break
@@ -32,3 +30,50 @@ switch (temperature) {
         break
     
 }
+
+// Exercise 2: Divisibility Check
+// 1. Write a code using if else statements to check if a number is
+// divisible by 2, 3, or both:
+// a. • If the number is divisible both by 2 and 3, print
+// i. "Divisible by both."
+// b. • If the number is divisible by only 2, print"Divisible by 2."
+// c. • If the number is divisible by only 3, print"Divisible by 3."
+// d. • If the number is not divisible by either, print "Not divisible by 2 or
+// 3."
+
+// 2. Re-write the code using switch Statements.
+let number = 7;
+if(number % 2 == 0 && number % 3 == 0){
+    console.log("Divisible by both.")
+} else if(number % 2 == 0){
+    console.log("Divisible by 2.")
+}else if(number % 3 == 0){
+    console.log("Divisible by 3.")
+}else{
+    console.log("Not divisible by 2 or 3")
+}
+
+switch (true){
+    case (number % 2 == 0 && number % 3 == 0):
+        console.log("Divisible by both.")
+        break
+    case (number % 2 == 0):
+        console.log("Divisible by 2.")
+        break
+    case (number % 3 == 0):
+        console.log("Divisible by 3.")
+        break
+    default:
+        console.log("Not divisible by 2 or 3")
+}
+
+
+// Exercise 3: For loops
+// 1. Print numbers from 1 to 10.
+// 2. Print all even numbers between 1 and 20.
+// 3. Calculate the sum of all numbers from 1 to 100 and print the result. Hint: ‘+=’
+// 4. const numbers = [1, 2, 3, 4, 5]. Given the array of numbers, print each
+// element to the console.
+// 5. const numbers = [3,7, 2, 5, 10, 6]. Given the array of numbers, find and print
+// the largest number.
+// #Hint: current value and previous value.
