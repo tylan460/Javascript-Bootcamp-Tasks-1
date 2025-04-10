3348832368b0370a5fb41c11f5eb1b7de14677ca
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-04-10 19:19:50 +0200

    completed exercise1

---
+++ variables.js
@@ -0,0 +1,20 @@
+
+
+/*
+Exercise 1: Variable Declaration
+1. Declare a variable ‘name’ and assign your name to it.
+2. Declare a variable ‘age’ and assign your age to it.
+3. Declare a variable ‘isStudent’ and assign it to a boolean value indicating
+whether you are a student or not.
+4. Print all three variables.
+*/
+
+let name = "Tylan";
+
+let age = '23'
+
+let isStudent = true;
+
+console.log("Name: " + name)
+console.log("Age: " + age)
+console.log("Is a student: " + isStudent)
