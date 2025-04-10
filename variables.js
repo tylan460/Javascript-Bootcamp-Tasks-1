8922d3989f73d52200eaa90f142d0885a0c60386
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-04-10 20:23:40 +0200

    completed exercis3

--- variables.js
+++ variables.js
@@ -37,3 +37,25 @@ favoriteColour = "red";
 console.log(favoriteColour);
 
 
+
+/*
+Exercise 3: Arithmetic
+Operations:
+1. Declare two variables ‘num1’ and ‘num2’ and assign them any two
+numbers.
+2. Declare variables to store the sum, difference, product, and quotient of
+num1 and num2.
+3. Print the results of these arithmetic operations.
+
+*/
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2
+let difference = num1 - num2
+let quotient = num1 / num2;
+
+console.log(sum)
+console.log(difference)
+console.log(quotient)
