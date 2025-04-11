6d97685d5a7f1fbf85841f77844e78d23f70217c
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-04-11 07:04:01 +0200

    completed exercise3(for loops)

--- conditional-statements-and-loops.js
+++ conditional-statements-and-loops.js
@@ -77,3 +77,43 @@ switch (true){
 // 5. const numbers = [3,7, 2, 5, 10, 6]. Given the array of numbers, find and print
 // the largest number.
 // #Hint: current value and previous value.
+
+
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log("exercise 3.2")
+for(let i = 1; i < 20; i++){
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
+
+
+let sum = 0;
+
+for(let i = 1; i <= 100; i++){
+    sum += i
+}
+
+console.log(sum)
+
+
+const numbers = [1, 2, 3, 4, 5]
+
+for(let i = 0; i < numbers.length; i++){
+    numbers[i]
+}
+
+const numbers2 = [3,7, 2, 5, 10, 6]
+
+let biggestNumber = 0;
+
+for(let i = 0; i < numbers2.length; i++){
+    if(biggestNumber <= numbers2[i]){
+        biggestNumber = numbers2[i]
+    }
+}
+console.log(biggestNumber)
